245fa9d919e75d6f93a1baa88791b7133c6fc4d0
AndrewTian12 <78053133+AndrewTian12@users.noreply.github.com>
2023-11-29 17:12:34 +0000

    new actions and actually ends game

--- dominion.py
+++ dominion.py
@@ -16,6 +16,9 @@ class DominionPlayer:
         self.hand = []
         self.deck = []
         self.discard_pile = []
+        self.coins = 0
+        self.num_provinces_purchased = 0
+        self.game = None
 
     def draw_cards(self, num_cards):
         for _ in range(num_cards):
@@ -33,6 +36,24 @@ class DominionPlayer:
     def display_hand(self):
         print(f"{self.name}'s hand: {[str(card) for card in self.hand]}")
 
+    def calculate_victory_points(self):
+        vp = sum(card.cost for card in self.deck + self.hand + self.discard_pile if card.card_type == "Victory")
+        return vp
+
+    def purchase_card(self, card_name):
+        affordable_cards = [card for card in self.game.supply if card.name == card_name and card.cost <= self.coins]
+        if affordable_cards:
+            chosen_card = random.choice(affordable_cards)
+            self.discard_pile.append(chosen_card)
+            self.coins -= chosen_card.cost
+
+            # Example: Track the number of Provinces purchased
+            if chosen_card.name == "Province":
+                self.num_provinces_purchased += 1
+
+            return chosen_card
+        return None
+
 class DominionGame:
     def __init__(self, players):
         self.players = players
@@ -40,7 +61,6 @@ class DominionGame:
         self.current_player_index = 0
 
     def create_supply(self):
-        # Add more card types as needed
         supply = [
             DominionCard("Copper", 0, "Treasure"),
             DominionCard("Silver", 3, "Treasure"),
@@ -50,12 +70,16 @@ class DominionGame:
             DominionCard("Province", 8, "Victory"),
             DominionCard("Smithy", 4, "Action", action=self.draw_action),
             DominionCard("Village", 3, "Action", action=self.draw_action),
+            DominionCard("Cellar", 2, "Action", action=self.cellar_action),
+            DominionCard("Market", 5, "Action", action=self.market_action),
+            DominionCard("Workshop", 3, "Action", action=self.workshop_action),
             # Add more cards...
         ]
         return supply
 
     def start_game(self):
         for player in self.players:
+            player.game = self
             self.initialize_player_deck(player)
             player.draw_cards(5)
 
@@ -94,17 +118,70 @@ class DominionGame:
     def draw_action(self, player):
         player.draw_cards(3)
 
+    def cellar_action(self, player):
+        print(f"{player.name} played Cellar. Discarding cards and drawing replacements.")
+        num_discards = len([card for card in player.hand if card.card_type != "Victory"])
+        discarded_cards = random.sample(player.hand, num_discards)
+        for card in discarded_cards:
+            player.hand.remove(card)
+            player.discard_pile.append(card)
+        player.draw_cards(num_discards)
+
+    def market_action(self, player):
+        print(f"{player.name} played Market. Drawing a card, gaining an action, and getting +1 buy and +1 coin.")
+        player.draw_cards(1)
+        player.draw_action(1)
+        player.num_buys += 1
+        player.coins += 1
+
+    def workshop_action(self, player):
+        print(f"{player.name} played Workshop. Gaining a card costing up to 4 coins.")
+        card_to_gain = self.buy_card_up_to_cost(4)
+        if card_to_gain:
+            player.discard_pile.append(card_to_gain)
+            print(f"{player.name} gained {card_to_gain}")
+
     def buy_card(self, card_name, player):
         affordable_cards = [card for card in self.supply if card.name == card_name and card.cost <= 6]
         if affordable_cards:
             chosen_card = random.choice(affordable_cards)
             if player.deck:
                 return chosen_card
+        return None
+
+    def buy_card_up_to_cost(self, max_cost):
+        affordable_cards = [card for card in self.supply if card.cost <= max_cost]
+        if affordable_cards:
+            chosen_card = random.choice(affordable_cards)
+            return chosen_card
+        return None
+
+    def determine_winner(self):
+        winners = []
+        max_vp = max(player.calculate_victory_points() for player in self.players)
+        for player in self.players:
+            if player.calculate_victory_points() == max_vp:
+                winners.append(player)
+        return winners
+
+    def game_over(self):
+        provinces_purchased = any(player.num_provinces_purchased >= 3 for player in self.players)
+        province_supply_empty = not any(card.name == "Province" for card in self.supply)
+        return provinces_purchased or province_supply_empty
 
     def run(self, num_turns):
         self.start_game()
         for _ in range(num_turns):
             self.play_turn()
+            if self.game_over():
+                break
+
+        # Game Over - Determine Winner
+        winners = self.determine_winner()
+        if len(winners) == 1:
+            print(f"\n{winners[0].name} wins!")
+        else:
+            print("\nIt's a tie!")
 
 # Example usage
 player1 = DominionPlayer("Player 1")
@@ -112,4 +189,4 @@ player2 = DominionPlayer("Player 2")
 players = [player1, player2]
 
 dominion_game = DominionGame(players)
-dominion_game.run(5)
+dominion_game.run(20)  # Increase the number of turns to allow more time for the game to end
